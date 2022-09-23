5fd6fc9b805c5182626fa859f052a927ebc34ef4
Manuka <manukajayarathne.coma@gmail.com>
2022-09-23 22:21:46 +0530

    update to get user input

--- qr-Work/qr-work.py
+++ qr-Work/qr-work.py
@@ -1,6 +1,7 @@
 import qrcode
 
-data = "https://github.com/AshanManuka"
-
+#data = "https://github.com/AshanManuka"
+data = input("Enter some content which include in Your QR : ")
+print(data)
 img = qrcode.make(data)
 img.save("qr-one.jpg")
